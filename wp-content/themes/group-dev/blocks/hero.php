75bf8c8e76609e5476fffd2c0b05a1b2d1d696b5
Daniel Rojas <danielrojas243@gmail.com>
2022-11-10 10:30:42 -0500

    hero

--- wp-content/themes/group-dev/blocks/hero.php
+++ wp-content/themes/group-dev/blocks/hero.php
@@ -4,8 +4,25 @@ $title = get_field("bh_title");
 
 ?>
 
-<div>HOALLALAffffffff
-  <?php if($title): ?>
-    <h1><?php echo $title ?></h1>
-  <?php endif; ?>
-</div>
+<section class="hero">
+  <div class="wrapper">
+    <div class="hero__heading">
+      <div class="badge">
+        Smart solution
+      </div>
+      <div class="hero__title">
+        We design better solution for your <span class="animation-lleter">dream</span>
+      </div>
+      <div class="hero__copy">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem deleniti commodi, modi autem dolorem sequi aliquid at? Eum libero dicta dolores sapiente expedita! Fuga molestiae perferendis porro accusamus, rem minus!
+      </div>
+      <a href="" class="button button--primary">
+        Learn more
+        <i></i>
+      </a>
+    </div>
+    <figure class="hero__image">
+      <img class="w-full" src="<?= UPLOAD_DIR ?>/drawdsgn.jpeg" alt="">
+    </figure>
+  </div>
+</section>
