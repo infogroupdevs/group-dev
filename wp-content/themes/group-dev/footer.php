212bfc503248f4cd576548dd3d4e7480faa1d2e7
Daniel Rojas <danielrojas243@gmail.com>
2022-11-14 20:03:29 -0500

    finish footer

--- wp-content/themes/group-dev/footer.php
+++ wp-content/themes/group-dev/footer.php
@@ -11,16 +11,37 @@
 
 ?>
 
-<footer class="footer bg-secondary">
-    <div class="d-flex pt-150 pb-100">
-        <div>
-            <h2>Let's collaborate</h2>
-            <div>
-            
-            </div>
-        </div>
-        <div></div>
-    </div>
+<footer class="footer bg-secondary py-50 lg:py-100">
+	<div class="wrapper">
+		<div class="lg:grid-50">
+			<div class="">
+				<h2 class="text-white mb-100">Let's collaborate</h2>
+				<div class="text-white mb-30">LOGO</div>
+				<p class="text-white">Powered by SocioLib.</p>
+			</div>
+			<div>
+				<p class="text-white mb-30">
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.
+				</p>
+				<h3 class="text-primary mb-50">Chat our expert</h3>
+				<div>
+					<div class="lg:grid-50">
+						<div>
+							<p class="text-white mb-10">CONTACTO</p>
+							<p class="text-white mb-5">hello@roll.er</p>
+							<p class="text-white mb-5">+987 654 321</p>
+							<p class="text-white">+123 456 789</p>
+						</div>
+						<div>
+						 <p class="text-white mb-5 mt-30">Amphitheatre Parkway, </p>
+						 <p class="text-white mb-5">Mountain View, </p>
+						 <p class="text-white">California, USA 94043</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 </footer>
 
 
